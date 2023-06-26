6ca72fbd89d114656805a6eec27dfc7b3cbf7992
Clement <clementmphethi@gmail.com>
2023-06-26 13:57:33 +0000

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as Error:
+        print("Exception: {}".format(Error), file=sys.stderr)
+        result = None
+    return result
