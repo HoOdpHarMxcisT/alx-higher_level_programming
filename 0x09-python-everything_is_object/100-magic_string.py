59ef6a3ace00f82d6b678abca6ab6849f0eb2150
Clement <clementmphethi@gmail.com>
2023-07-04 19:48:43 +0000

    error fixed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["BestSchool" for i in range(magic_string.count)])
-
+def magic_string(new_list=[]):
+    new_list.append("BestSchool")
+    return ', '.join(new_list)
