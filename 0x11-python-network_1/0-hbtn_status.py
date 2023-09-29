016fa719020f4ff2fcb888c4cf75140db1f94a96
Clement <clementmphethi@gmail.com>
2023-09-29 12:22:23 -0700

    error fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
-from urllib import request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    req = request.Request(url)
-    with request.urlopen(req) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
-        to_print = "Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(body.__class__, body, body.decode())
-        print(to_print)
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 
+
+import sys
 import requests
 
+
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    body = requests.get(url).text
-    to_print = "Body response:\n\t- type: {}\n\t- content: {}\
-".format(type(body), body, body)
-    print(to_print)
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
